adaf51f166a9e9ac61cc1ef78a75d63884bf3957
betulkaplan-amplo <99176340+betulkaplan-amplo@users.noreply.github.com>
2023-03-26 16:09:57 +0200

    close modal onblur and refactor layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
 import React from "react";
 
+const menuItems = [
+  {
+    name: "Home",
+    route: "/",
+  },
+  {
+    name: "Posts",
+    route: "post",
+  },
+];
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="grid grid-cols-10 h-screen">
-      <div className="col-span-1 p-5 bg-gray-800 text-white ">
-        <h2 className="text-2xl mb-5">Advise Me</h2>
+      <div className="col-span-1 bg-gray-800 text-white ">
+        <h2 className="text-2xl mb-5 pl-5 my-5">Advise Me</h2>
         <ul>
-          <Link href={"/"}>
-            <li className="py-2 hover:bg-gray-700">Home</li>
-          </Link>
-          <Link href={"post"}>
-            <li className="py-2 hover:bg-gray-700">Posts</li>
-          </Link>
-          <li className="py-2 hover:bg-gray-700">Dashboard</li>
-          <li className="py-2 hover:bg-gray-700">Saved</li>
+          {menuItems.map((item) => (
+            <Link key={item.name} href={item.route}>
+              <li className="py-2 pl-5 hover:bg-gray-700">{item.name}</li>
+            </Link>
+          ))}
         </ul>
       </div>
 
--- components/modals/SimpleModal.tsx
+++ components/modals/SimpleModal.tsx
@@ -20,8 +20,14 @@ function SimpleModal({ isOpen, onClose, children }: SimpleModalProps) {
   }, [onClose]);
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <div className="p-2 flex flex-col items-end bg-white rounded-lg border-2 border-blue-300">
+    <div
+      onClick={onClose}
+      className="fixed inset-0 bg-opacity-25 backdrop-blur-sm flex justify-center items-center"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="p-2 flex flex-col items-end bg-white rounded-lg border-2 border-blue-300"
+      >
         <button onClick={onClose} className="m-2">
           X
         </button>
