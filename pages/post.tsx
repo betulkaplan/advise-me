886ef5e13dcadbe567242c414f4e96d0838d96c7
betulkaplan-amplo <99176340+betulkaplan-amplo@users.noreply.github.com>
2023-03-26 15:10:10 +0200

    add create post modal

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,5 +1,5 @@
 import LoadingWrapper from "@/components/LoadingWrapper";
-import SimpleModal from "@/components/modals/SimpleModal";
+import CreatePostModal from "@/components/modals/CreatePostModal";
 import PostCard from "@/components/PostCard";
 import { PostWithuser } from "@/types/commons";
 import useAxios from "axios-hooks";
@@ -23,96 +23,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <SimpleModal isOpen={isOpenModal} onClose={() => setIsOpenModal(false)}>
-          <form
-            onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-              e.preventDefault();
-              const title: any = e.currentTarget.elements[0];
-              const createdById: any = e.currentTarget.elements[1];
-              fetch("api/post", {
-                method: "POST",
-                body: JSON.stringify({
-                  title: title.value,
-                  createdById: createdById.value,
-                }),
-                headers: {
-                  "Content-Type": "application/json",
-                },
-              })
-                .then((res) => res.json())
-                .then((res) => {
-                  toast.success("User is added!");
-                  refetchUsers();
-                });
-            }}
-            className="bg-white"
-          >
-            <h1 className="text-gray-800 font-bold text-2xl mb-1">
-              Create Post
-            </h1>
-            <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-                />
-              </svg>
-              <input
-                className="pl-2 outline-none border-none"
-                type="text"
-                name=""
-                id=""
-                placeholder="Title"
-              />
-            </div>
-            <div className="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                />
-              </svg>
-              <input
-                className="pl-2 outline-none border-none"
-                type="text"
-                name=""
-                id=""
-                placeholder="User"
-              />
-            </div>
-            <button
-              type="submit"
-              className="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2"
-            >
-              Submit
-            </button>
-          </form>
-        </SimpleModal>
+      <main className="p-3">
+        <CreatePostModal
+          isOpen={isOpenModal}
+          onClose={() => setIsOpenModal(false)}
+          onCreate={() => {
+            setIsOpenModal(false);
+            refetchUsers();
+          }}
+        />
+
         <button
           onClick={() => setIsOpenModal(true)}
-          className="bg-blue-500 p-2 m-2 text-white rounded-md"
+          className="bg-blue-500 p-2 text-white rounded-md"
         >
           Create Post
         </button>
-        <h3>Posts</h3>
-        <div className="p-5 grid grid-cols-5 gap-3">
+        <div className="grid grid-cols-5 gap-3 mt-2">
           <LoadingWrapper isLoading={loading}>
             {posts?.map((post) => {
               return <PostCard key={post.id} post={post} />;
