0dee434df5897b27c784ffdecbfad632122292b3
betulkaplan-amplo <99176340+betulkaplan-amplo@users.noreply.github.com>
2023-03-26 14:50:03 +0200

    create post

--- components/modals/SimpleModal.tsx
+++ components/modals/SimpleModal.tsx
@@ -22,12 +22,12 @@ function SimpleModal({
   }, [onClose]);
   if (!isOpen) return null;
   return (
-    <div className=" fixed inset-0 bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <div className="p-2 flex flex-col items-end bg-white rounded-lg">
+    <div className="fixed inset-0 bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+      <div className="p-2 flex flex-col items-end bg-white rounded-lg border-2 border-blue-300">
         <button onClick={onClose} className="m-2">
           X
         </button>
-        <div className="bg-blue-400  min-w-[400px]">{children}</div>
+        <div className="p-2 min-w-[400px]">{children}</div>
       </div>
     </div>
   );
